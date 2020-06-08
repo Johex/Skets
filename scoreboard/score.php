4c139d56abf39c980e8e3554c9382a536caf1f32
Matisse <matisse>
2020-06-08 13:48:13 +0200

    scoreboard template

---
+++ scoreboard/score.php
@@ -0,0 +1,20 @@
+<div>
+    <table style="width:100%">
+        <tr>
+            <th>Name</th>
+            <th>Score</th>
+        </tr>
+        <tr>
+            <td>Matisse</td>
+            <td>14</td>
+        </tr>
+        <tr>
+            <td>Herman</td>
+            <td>10</td>
+        </tr>
+    </table>
+</div>
+
+<?php
+?>
+
