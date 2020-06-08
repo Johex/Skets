9f0eacb2b43177743870229f675a88d37085ea17
Romanterp <romanterpstra@gmail.com>
2020-06-08 15:09:23 +0200

    scoreboard done

--- scoreboard/score.php
+++ scoreboard/score.php
@@ -1,28 +1,23 @@
-<div>
-    <table style="width:50%">
-        <tr>
-            <th>Name</th>
-            <th>Score</th>
-        </tr>
-        <tr>
-            <td>Matisse</td>
-            <td>14</td>
-        </tr>
-        <tr>
-            <td>Roman</td>
-            <td>10</td>
-        </tr>
-    </table>
-</div>
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Score</th>
+    </tr>
 
-<?php
-$string = file_get_contents("score.json");
-$json_a=json_decode($string,true);
+    <?php
+    $string = file_get_contents("score.json");
+    $json_a = json_decode($string,true);
+    arsort($json_a);
 
-foreach ($json_a as $key => $value){
-    echo  $key . ':' . $value . "<br>";
-}
-?>
-<?php
-?>
+    foreach ($json_a as $key => $value){
+        echo "<tr>
+                    <td>$key</td>
+                    <td>$value</td>
+              </tr>";
+    }
+    ?>
+</table>
+
+
+
 
